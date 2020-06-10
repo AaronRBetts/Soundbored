06dfdeb43e5cfd6182200ff3b3f9d871010fe9e3
AaronRBetts <aaronrbetts@gmail.com>
2020-06-10 12:32:17 +0300

    Updates

---
+++ precache-manifest.9a83a1e321966cb2ce8f1f34b58ad81c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ee8bc045d87aab6e4ade08974a59c27",
+    "url": "/Soundbored/index.html"
+  },
+  {
+    "revision": "428aad2f3b80b3f3950c",
+    "url": "/Soundbored/static/css/main.3e7d667e.chunk.css"
+  },
+  {
+    "revision": "6501fbcf2de92897eeae",
+    "url": "/Soundbored/static/js/2.1fa54ed1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Soundbored/static/js/2.1fa54ed1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "428aad2f3b80b3f3950c",
+    "url": "/Soundbored/static/js/main.58dc0f79.chunk.js"
+  },
+  {
+    "revision": "131ea53e72110b058910",
+    "url": "/Soundbored/static/js/runtime-main.83951474.js"
+  }
+]);
